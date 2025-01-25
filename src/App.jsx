c66ef4c044be4427fbfbda48e91df7ddaff30104
Harshadakhot34 <hkhot30@gmail.com>
2025-01-25 20:55:07 +0530

    Frontend UI is done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
-import Navbar from './components/Navbar'
-import Banner from './components/Banner'
-import Footer from './components/Footer'
+import Home from './home/Home'
+import Courses from '../src/courses/Courses'
+import { Route, Routes } from "react-router-dom"
+import Signup from './components/Signup'
+
 const App = () => {
   return (
 <>
-<Navbar/> 
-<Banner/>
-<Footer/>
+{/* <Home/>
+<Course/> */}
+<div className='dark:bg-slate-900 dark:text-white'>
+  
+<Routes>
+  <Route path='/' element={<Home/>}></Route>
+  <Route path='/course' element={<Courses/>}></Route>
+  <Route path='/signup' element={<Signup/>}></Route>
+
+  
+
+
+</Routes>
+</div>
 </>
 
   )
